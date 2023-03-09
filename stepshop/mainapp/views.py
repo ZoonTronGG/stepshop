309085c6ca6cfbbd059e74ba753ad2ea4e0046eb
Титоренко Артём <@itstep.loc>
2023-03-09 21:22:48 +0600

    lesson_05

--- stepshop/mainapp/views.py
+++ stepshop/mainapp/views.py
@@ -1,12 +1,18 @@
 from django.shortcuts import render
 
+from mainapp.models import Product, ProductCategory
 
-# Create your views here.
+
 def products(request):
-    title = 'продукты/каталог'
+    title = 'продукты'
+
+    products = Product.objects.all()[:2]
+    categories = ProductCategory.objects.all()
 
     context = {
         'title': title,
+        'products': products,
+        'categories': categories,
     }
 
     return render(request, 'products.html', context)
