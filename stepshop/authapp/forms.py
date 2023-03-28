5ec8764b1dbce9da2f238f54e79fd32c38a50fad
Титоренко Артём <@itstep.loc>
2023-03-28 20:50:45 +0600

    lesson_08

--- stepshop/authapp/forms.py
+++ stepshop/authapp/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm, UserChangeForm
 
 from authapp.models import ShopUser
 
@@ -38,5 +38,26 @@ class ShopUserRegisterForm(UserCreationForm):
     def clean_avatar(self):
         data = self.cleaned_data['avatar']
         if data.size > 3145728:
-            raise ValidationError('Размер файла не должен превышать 3 Мб')
+            raise forms.ValidationError('Размер файла не должен превышать 3 Мб')
+        return data
+
+
+class ShopUserEditForm(UserChangeForm):
+    class Meta:
+        model = ShopUser
+        fields = ('username', 'first_name', 'password', 'email', 'avatar', 'age')
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field_name, field in self.fields.items():
+            field.widget.attrs['class'] = 'form-control py-4'
+            field.help_text = ''
+        self.fields['avatar'].help_text = 'Загружайте изображения в формате PNG, JPG или JPEG'
+        self.fields['age'].help_text = 'Укажите свой возраст'
+        self.fields['password'].widget = forms.HiddenInput()
+
+    def clean_age(self):
+        data = self.cleaned_data['age']
+        if data < 18:
+            raise forms.ValidationError('Вы слишком молоды')
         return data
