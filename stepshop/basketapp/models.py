35b776cf6294733b71bb7297b100fcc98f2d25fc
Титоренко Артём <@itstep.loc>
2023-03-30 21:41:32 +0600

    lesson_09

---
+++ stepshop/basketapp/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+from mainapp.models import Product
+from stepshop import settings
+
+
+class Basket(models.Model):
+    user = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name='basket',
+    )
+    product = models.ForeignKey(
+        Product,
+        on_delete=models.CASCADE,
+         )
+    quantity = models.PositiveIntegerField(verbose_name='количество', default=0)
+    add_datetime = models.DateTimeField(verbose_name='время', auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.user.username} | {self.product.name}'
+
+    class Meta:
+        verbose_name = 'корзина'
+        verbose_name_plural = 'корзины'
+        ordering = ['-add_datetime']
