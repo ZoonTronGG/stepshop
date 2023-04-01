9d726bbc823beb1207cdc093f3e4d6a999d575bb
Titorenko Artyom <unknown@mail.ru>
2023-04-01 18:47:38 +0600

    lesson_08_HW

--- stepshop/authapp/views.py
+++ stepshop/authapp/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
 
-from .forms import ShopUserLoginForm, ShopUserRegisterForm
+from .forms import ShopUserLoginForm, ShopUserRegisterForm, ShopUserEditForm
 
 
 def login(request):
@@ -55,3 +55,23 @@ def register(request):
     }
 
     return render(request, 'auth/register.html', context)
+
+
+def edit(request):
+    title = 'редактирование'
+
+    if request.method == 'POST':
+        edit_form = ShopUserEditForm(request.POST, request.FILES, instance=request.user)
+
+        if edit_form.is_valid():
+            edit_form.save()
+            return HttpResponseRedirect(reverse('auth:edit'))
+    else:
+        edit_form = ShopUserEditForm(instance=request.user)
+
+    context = {
+        'title': title,
+        'edit_form': edit_form,
+    }
+
+    return render(request, 'auth/edit.html', context)
